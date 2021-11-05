c83ae0f52cc9c5514749a3459b058e37738349d1
Me <jlhofstra@yahoo.com>
2021-11-06 07:03:01 +0900

    added a field in the model to post images

--- ablog/theblog/models.py
+++ ablog/theblog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User 
 from django.urls import reverse
 from datetime import datetime, date
+from ckeditor.fields import RichTextField
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -16,9 +17,12 @@ class Category(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255)
+    header_image = models.ImageField(null = True, blank = True, upload_to= "images/")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.CharField(max_length=255, default='uncategorized')
-    body = models.TextField()
+    snippet = models.CharField(max_length=255, default='Click link above to read...')
+    body = RichTextField(blank = True, null = True)
+    # body = models.TextField()
     publication_date = models.DateTimeField(auto_now_add=True)
     likes = models.ManyToManyField(User, related_name='blog_posts')
 
@@ -37,7 +41,10 @@ class EditPost(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255)
     # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    snippet = models.CharField(max_length=255, default='Click link above to read...')
     body = models.TextField()
+    header_image = models.ImageField(null = True, blank = True, upload_to= "images/")
+
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
