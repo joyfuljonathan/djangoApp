3dbcf139af3be18c115feb8aed54204362c43317
unknown <jonathan@joyful-cube.com>
2023-03-29 16:12:25 +0900

    adding the painting repository

--- ablog/ablog/settings.py
+++ ablog/ablog/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'theblog',
     'members',
     'ckeditor',
+    # 'django.contrib.staticfiles'
     # 'import-export',
 ]
 
@@ -126,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = (
